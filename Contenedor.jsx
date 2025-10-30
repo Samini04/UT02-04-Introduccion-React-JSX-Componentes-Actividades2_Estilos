db12dca5ea23cc8223bf41df6d120d7ed5d29820
Samini04 <abdsam@alu.edu.gva.es>
2025-10-30 17:22:25 +0100

    Delete Contenedor.jsx

--- Contenedor.jsx
+++
@@ -1,9 +0,0 @@
-function Contenedor({ children }) {
-  return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-8">
-      {children}
-    </div>
-  );
-}
-
-export default Contenedor;
